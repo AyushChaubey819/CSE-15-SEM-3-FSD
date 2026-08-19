cf5a5f48fa55c97dddc8b7a078558c192535a441
AyushChaubey819 <i.ayushchaubey@gmail.com>
2026-08-19 11:39:52 +0530

    OS Features of Node v26

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log("hello  world" ) ;
+// const shyam = require("./xyz") ; // reuire is used to immpkrt the  function  
+// console.log("hello",shyam)
+
+const os = require("os") ;
+console.log("Free Memory: ",os.freemem());
+console.log("Total Memory: ",os.totalmem());
+console.log("Platform: ",os.platform());
+console.log("Architecture: ",os.arch());
+console.log("CPU Info: ",os.cpus());
+console.log("Home Directory: ",os.homedir());
+console.log("Hostname: ",os.hostname());
